9d51a340aabb934e521890038d517e569eb8df55
Andrew Wang <huskywang@AndrewdeMacBook-Air.local>
2022-06-16 00:29:10 +0800

    baseline

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
+// Created by Andrew
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container1}>
+        
+        <Text style={{ color: '#fff', fontSize: 30 }}>Hello World!</Text>
+      </View>
+      <View style={styles.container2}>
+        
+        <Text>我的第一個React Native程式！！</Text>
+        <Text>This is my first React Native！！</Text>
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,6 +24,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  container1: {
+    flex: 2,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },  
+  container2: {
+    flex: 3,
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
   },
